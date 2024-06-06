27b7ed177089ed5aef81c2f9a734f093a1cbae0e
Peachplace Deisler <avadeisler@gmail.com>
2024-06-06 17:15:29 -0500

    houses

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import houses from "./houses.png";
+
 
 const Header = () => {
   return (
     <header className="">
-      <div className="">
-        <Link className="" to="/">
-          <a href="https://flyclipart.com/talk-to-your-mpp-ops-row-of-houses-clipart-255763" title="Talk To Your Mpp Ops - Row Of Houses Clipart"><img src="https://flyclipart.com/thumb2/talk-to-your-mpp-ops-255763.png" width="350" /></a>
-        </Link>
+       <img src={houses} alt="houses" />
+        <div className="">
+        {/* <Link className="" to="/">
+        </Link> */}
         <p className="" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
         </p>
       </div>
