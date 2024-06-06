db34d0b2a8dd3db0fa070202e3b10eb108a8d8b9
Peachplace Deisler <avadeisler@gmail.com>
2024-06-05 20:04:34 -0500

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { useQuery } from '@apollo/client';
 
+import Header from '../components/Header';
 import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
 
 // import query
 import {QUERY_POST} from '../utils/queries'
 
 function Home(){
+    const element = <h1> What's new in your Hood??</h1>;
     const { loading, data } = useQuery(QUERY_POST)
     const postData = data?.getAllPosts || []
     if(loading)return <div> Loading </div>
@@ -17,3 +21,9 @@ function Home(){
 }
 
 export default Home 
+
+// header w/ houses (includes login button)
+// clickable mailbox with neighboorhood events
+// comment feed from main
+// sidebar
+// footer
