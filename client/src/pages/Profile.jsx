b2746a97286d2a6a464a12dd71764741e0d51179
Nicodemus <pronic@live.com>
2024-06-06 11:43:02 -0500

    profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,79 +3,39 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div className="container">
-            <div className="card">
-                <div className="background">
-                    <img src="assets/profile.jpg" alt="background" />
+        <div className="container">       
+            <article className="card">
+                <div className='background'>
+                    <img src="assets/profile.jpeg" alt="profile" />
                 </div>
-                <div className="content">
-                    <h2>Jason Alexander</h2>
+                <div className='content'>
+                    <h2>Profile name &#127468;&#127463;</h2>
                     <p>
-                        I'm JavaScript full-stack engineer, love working with <em>Gardening</em> and <em>Plumbing</em>.
+                        Brief intro of person here  
+                        <a href="email ad" title="Google">DM</a>
                     </p>
-                    <ul className="social-icons">
-                        <li>
-                            <a href="#twitter" title="Twitter">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter">
-                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#instagram" title="Instagram">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#threads" title="Threads">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M19 7.5c-1.333 -3 -3.667 -4.5 -7 -4.5c-5 0 -8 2.5 -8 9s3.5 9 8 9s7 -3 7 -5s-1 -5 -7 -5c-2.5 0 -3 1.25 -3 2.5c0 1.5 1 2.5 2.5 2.5c2.5 0 3.5 -1.5 3.5 -5s-2 -4 -3 -4s-1.833 .333 -2.5 1"></path>
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#linkedin" title="Linkedin">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin">
-                                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                                    <rect x="2" y="9" width="4" height="12"></rect>
-                                    <circle cx="4" cy="4" r="2"></circle>
-                                </svg>
-                            </a>
-                        </li>
+                    <p>Helping with:</p>
+                    <ul className="chips">
+                        <li className="chip">Skill1</li>
+                        <li className="chip">Skill2</li>
+                        <li className="chip">Skill3</li>
+                        <li className="chip">Zipcode</li>
                     </ul>
+                    <div className="action-buttons">
+                        <a href="#book-a-mentor">
+                            Book a Service
+                        </a>
+                        <a href="#learn-more" className="secondary">
+                            message
+                        </a>
+                    </div>
                 </div>
-            </div>
+            </article>
         </div>
     );
 };
 
 export default Profile;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 
 
 
 
