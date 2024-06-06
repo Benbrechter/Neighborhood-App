6af9980f91b2c874bea29e9c70731b1c2532038a
Benjamin Brechter <benbrechter3@gmail.com>
2024-06-06 18:30:13 -0500

    Debugging main with tye

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,19 @@
-// import { useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 
 import Main from '../components/Main/index';
 // import query
-// import {QUERY_POST} from '../utils/queries'
+import {QUERY_POST} from '../utils/queries'
 
 function Home(){
-    const element = <h1 className= 'homeHeader'> What's new in your Hood??</h1>;
-    // const { loading, data } = useQuery(QUERY_POST)
-    // const postData = data?.getAllPosts || []
-    // if(loading)return <div> Loading </div>
+    const { loading, data } = useQuery(QUERY_POST)
+    const postData = data?.getAllPosts || []
+    console.log(postData)
+    if(loading)return <div> Loading </div>
     return (
         <div>
-            <h1>{element}</h1>
-        <Main />
+            <h1 className= 'homeHeader'> What's new in your Hood?</h1>
+        <Main postData={postData}/>
         </div>
     )
 }
