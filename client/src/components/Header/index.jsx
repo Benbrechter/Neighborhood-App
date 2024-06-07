b1e1bd83a5ba24f8a5e7d653339ef1bf5fc894b3
Via Skillings <viaskillings@gmail.com>
2024-06-06 19:41:16 -0500

    pretty houses

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -18,9 +18,9 @@ const Header = () => {
     <header className="header">
       <div className="nav-container">
         <Link to="/">
-          <img src='https://i.postimg.cc/Cx78jd9y/houses.png' alt='houses' className="logo" />
+        {/* <a href='https://postimg.cc/687TK69z' target='_blank'><img src='https://i.postimg.cc/Cx78jd9y/houses.png' border='0' alt='houses'/></a> */}
+          {/* <img src='https://i.postimg.cc/Cx78jd9y/houses.png' alt='houses' className="logo" border='0'/> */}
         </Link>
-        <p className="title">Home</p>
       </div>
     </header>
   );
