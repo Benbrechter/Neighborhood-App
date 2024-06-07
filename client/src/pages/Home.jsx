115db4abc68a33d8b5399479065c0b3754541c95
Nicodemus <pronic@live.com>
2024-06-06 21:21:40 -0500

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
+<<<<<<< Updated upstream
 import { useQuery } from '@apollo/client';
 
+=======
+// import { useQuery } from '@apollo/client';
+// import Footer from '../components/Footer/index';
+>>>>>>> Stashed changes
 
-import Main from '../components/Main/index';
-// import query
-import {QUERY_POST} from '../utils/queries'
+// import Main from '../components/Main/index';
+// // import query
+// import {QUERY_POST} from '../utils/queries'
 
+<<<<<<< Updated upstream
 function Home(){
     const { loading, data } = useQuery(QUERY_POST)
     const postData = data?.getAllPosts || []
@@ -17,6 +23,21 @@ function Home(){
         </div>
     )
 }
+=======
+// export default function Home(){
+//     const { loading, data } = useQuery(QUERY_POST)
+//     const postData = data?.getAllPosts || []
+//     console.log(postData)
+//     // if(loading)return <div> Loading </div>
+//     return (
+//         <div>
+//             <h1 className= 'homeHeader'> What's new in your Hood? testing</h1>
+//         <Main postData={postData}/>
+//         <Footer/>
+//         </div>
+//     )
+// }
+>>>>>>> Stashed changes
 
 export default Home 
 
