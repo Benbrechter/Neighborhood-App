629fcd27bc789508a41fd4f4662ccd99da3c91bf
Nicodemus <pronic@live.com>
2024-06-06 21:56:48 -0500

    adding footer- conflict

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,43 +1,23 @@
-<<<<<<< Updated upstream
 import { useQuery } from '@apollo/client';
+import Footer from '../components/Footer/index';
 
-=======
-// import { useQuery } from '@apollo/client';
-// import Footer from '../components/Footer/index';
->>>>>>> Stashed changes
+import Main from '../components/Main/index';
 
-// import Main from '../components/Main/index';
-// // import query
-// import {QUERY_POST} from '../utils/queries'
+import {QUERY_POST} from '../utils/queries'
 
-<<<<<<< Updated upstream
-function Home(){
-    const { loading, data } = useQuery(QUERY_POST)
-    const postData = data?.getAllPosts || []
-    console.log(postData)
+ function Home(){
+    const { loading, data } = useQuery(QUERY_POST);
+    const postData = data?.getAllPosts || [];
+   
     if(loading)return <div> Loading </div>
     return (
         <div>
-            <h1 className= 'homeHeader'> What's new in your Hood?</h1>
+            <h1 className='homeHeader'> {"What's new in your Hood?"}</h1>
         <Main postData={postData}/>
+        <Footer/>
         </div>
     )
 }
-=======
-// export default function Home(){
-//     const { loading, data } = useQuery(QUERY_POST)
-//     const postData = data?.getAllPosts || []
-//     console.log(postData)
-//     // if(loading)return <div> Loading </div>
-//     return (
-//         <div>
-//             <h1 className= 'homeHeader'> What's new in your Hood? testing</h1>
-//         <Main postData={postData}/>
-//         <Footer/>
-//         </div>
-//     )
-// }
->>>>>>> Stashed changes
 
 export default Home 
 
